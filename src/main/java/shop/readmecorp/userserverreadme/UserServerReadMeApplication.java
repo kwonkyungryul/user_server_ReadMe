4a8712f409016c94c6c087edb8de4a2b7d9db529
kkr <kkr0787@naver.com>
2023-04-13 20:00:07 +0900

    initial commit

---
+++ src/main/java/shop/readmecorp/userserverreadme/UserServerReadMeApplication.java
@@ -0,0 +1,13 @@
+package shop.readmecorp.userserverreadme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServerReadMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServerReadMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/readmecorp/userserverreadme/UserServerReadMeApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.readmecorp.userserverreadme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServerReadMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
